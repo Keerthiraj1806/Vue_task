6f0aae5721b27d6a081be60e1578f5d4350a8dad
keerthi <keerthiraj1806@gmail.com>
2023-07-05 18:13:17 +0530

    first commit

---
+++ src/views/Tab.vue
@@ -0,0 +1,34 @@
+<script>
+//options
+export default{
+data(){
+    return {
+        items:[{Name:'Praveen',Age: 25,Company: 'Wipro'},
+            {Name:'Vino',Age:21,Company: 'Afcon'},
+            {Name:'Kavin',Age:28,Company:'TCS'},
+            {Name:'Nirmal',Age:20,Company:'CTS'}]
+    }
+}
+}
+</script>
+<template>
+    <table class="table">
+        <tr>
+            <th class="table">Name</th>
+            <th class="table">Age</th>
+            <th class="table">Company</th>
+        </tr>
+        <tr v-for="person in items">
+            <td class="table">{{ person.Name }}</td>
+            <td class="table">{{ person.Age }}</td>
+            <td class="table">{{ person.Company }}</td>
+        </tr>
+    </table>
+</template>
+
+<style scoped>
+    .table{
+        border: 2px black dotted;
+        text-align: center;
+    }
+</style>
---
+++ src/views/TableView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+//composition
+import { ref } from 'vue';
+const items = ref([{ Name:'Praveen',Age:25,Company: 'Wipro'}, { Name:'Kavin',Age:24,Company: 'Afcon'},{ Name:'vino',Age:26,Company: 'Kgr'}])
+</script>
+
+<template>
+    <table class="table">
+        <tr>
+            <th class="table">Name</th>
+            <th class="table">Age</th>
+            <th class="table">Company</th>
+        </tr>
+        <tr v-for="item in items">
+            <td class="table">{{ item.Name }}</td>
+            <td class="table">{{ item.Age }}</td>
+            <td class="table">{{ item.Company }}</td>
+        </tr>
+    </table>
+</template>
+
+<style scoped>
+    .table{
+        border: 2px;
+        border-color: black;
+        border-style: solid;
+        text-align: center;
+    }
+</style>
